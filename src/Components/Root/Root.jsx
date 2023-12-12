b8efcfc7d5ed8127a550af61acefe8eba023f99e
Ariful2634 <ariful2634@gmail.com>
2023-12-12 11:38:16 +0600

    finalized the portfolio

--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -1,16 +1,21 @@
 import { Outlet } from "react-router-dom";
 import SideBar from "../Home/SideBar";
+import Footer from "../Home/Footer";
 // import Home from "../Home/Home";
 // import Navbar from "../Home/Navbar";
 
 
 const Root = () => {
     return (
-        <div className="flex lg:flex-row flex-col max-w-6xl mx-auto ">
-           <SideBar></SideBar>
-          <div className="flex-1 lg:ml-16">
-          <Outlet></Outlet>
-          </div>
+        <div>
+            <div className="flex lg:flex-row flex-col max-w-6xl mx-auto ">
+                <SideBar></SideBar>
+                <div className="flex-1 lg:ml-16">
+                    <Outlet></Outlet>
+                </div>
+
+            </div>
+            <Footer></Footer>
         </div>
     );
 };
