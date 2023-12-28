5efafdf84a1d878387cfab42130c4a89ab18a09c
Ariful2634 <ariful2634@gmail.com>
2023-12-28 13:40:02 +0600

    update the project section

--- src/Components/Home/Projects.jsx
+++ src/Components/Home/Projects.jsx
@@ -1,66 +1,51 @@
 
 import 'aos/dist/aos.css';
+import techPic from '../../images/tech-hub-ss.png'
+import hotelPic from '../../images/hotel.png'
+import brandshopPic from '../../images/brand.png'
+import Project from './Project';
+
+
 const Projects = () => {
+
+    
+
+    const items = [
+        {
+            title: "The Tech Hub",
+            image: techPic,
+            liveLink: "https://tech-hub-c2612.web.app/",
+            repoLink: "https://github.com/Ariful2634/tech-hub-client",
+            description:
+                "It is a platform where users can discover and share the latest tech products. Users can submit new products, upvote or downvote existing products & post reviews for products.",
+        },
+        {
+            title: "Book My Hotel",
+            image: hotelPic,
+            liveLink: "https://hotel-booking-management-928e5.web.app/",
+            repoLink: "https://github.com/Ariful2634/hotel-booking-management-client",
+            description:
+                "A user-friendly hotel booking system that allows seamless reservations, providing a convenient platform to browse accommodations, select dates, and secure bookings with ease.",
+        },
+        {
+            title: "Tech Shop",
+            image: brandshopPic,
+            liveLink: "https://tech-shop-5a579.web.app/",
+            repoLink: "https://github.com/Ariful2634/brand-shop-client",
+            description:
+                "This is a tech gadget related website where user can see the brandwise product.User can see the product details. User can add a product to cart and can see the add product to my cart.",
+        },
+       
+    ];
+
     return (
-        <div data-aos="flip-left"
-        data-aos-easing="ease-out-cubic"
-        data-aos-duration="2000">
-            <div className="max-w-5xl mx-auto border p-3 mt-6 rounded">
-                <div className="flex lg:flex-row flex-col items-center justify-between ">
-                <h3 className="font-bold text-2xl">Project Name: <span className="text-green-600">The Tech Hub</span></h3>
-                <div className="flex gap-10 text-blue-600 font-bold">
-                        <a href="https://tech-hub-c2612.web.app/">Live Link</a>
-                        <a href="https://github.com/Ariful2634/tech-hub-client">Client Side</a>
-                        <a href="https://github.com/Ariful2634/tech-hub-server">Server Side</a>
-                    </div>
-                </div>
-                <p className="font-semibold mt-3"><span className="font-bold">Technology Used:</span> Reactjs, Tailwind CSS, Daisy UI, Material UI, MongoDB, Firebase, Stripe, Jwt etc.</p>
-                
-                    <p className="font-bold mt-3">Key Features :
-                    </p>
-                    <li>Created an Admin Route, Moderator Route, User Route. Admin can add users roles, add coupons.</li>
-                    <li>Users can post products and upvote the product.</li>
-                    <li>Moderators can accept products and marked them as featured.</li>
-                   
-                
-            </div>
-            <div className="max-w-5xl mx-auto border p-3 mt-6 rounded">
-                <div className="flex lg:flex-row flex-col items-center justify-between ">
-                <h3 className="font-bold text-2xl">Project Name: <span className="text-green-600">Book My Hotel</span></h3>
-                <div className="flex gap-10 text-blue-600 font-bold">
-                        <a href="https://hotel-booking-management-928e5.web.app/">Live Link</a>
-                        <a href="https://github.com/Ariful2634/hotel-booking-management-client">Client Side</a>
-                        <a href="https://github.com/Ariful2634/hotel-booking-management-server">Server Side</a>
-                    </div>
-                </div>
-                <p className="font-semibold mt-3"><span className="font-bold">Technology Used:</span> Reactjs, Tailwind CSS, Daisy UI, MongoDB, Firebase, Jwt etc.</p>
-                
-                    <p className="font-bold mt-3">Key Features :
-                    </p>
-                    <li>Users can view the rooms and room details.</li>
-                    <li>Users can book rooms.</li>
-                    <li>Users can view and post reviews for rooms.</li>
-                   
-                
-            </div>
-            <div className="max-w-5xl mx-auto border p-3 mt-6 rounded">
-                <div className="flex lg:flex-row flex-col items-center justify-between ">
-                <h3 className="font-bold text-2xl">Project Name: <span className="text-green-600">Tech Shop</span></h3>
-                <div className="flex gap-10 text-blue-600 font-bold">
-                        <a href="https://tech-shop-5a579.web.app/">Live Link</a>
-                        <a href="https://github.com/Ariful2634/tech-shop-client">Client Side</a>
-                        <a href="https://github.com/Ariful2634/tech-shop-server">Server Side</a>
-                    </div>
-                </div>
-                <p className="font-semibold mt-3"><span className="font-bold">Technology Used:</span> Reactjs, Tailwind CSS, Daisy UI, MongoDB, Firebase, etc.</p>
-                
-                    <p className="font-bold mt-3">Key Features :
-                    </p>
-                    <li>Users can see the brandwise product on clicking in a specific brand.</li>
-                    <li>Users can see the product details and also can add products.</li>
-                    <li>Users can update and delete the products.</li>
-                   
-                
+        
+        <div id="projects" className="w-11/12 md:w-10/12  mx-auto">
+          
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 py-12 gap-4">
+                {items.map((project, idx) => (
+                    <Project key={idx} project={project}></Project>
+                ))}
             </div>
         </div>
     );
