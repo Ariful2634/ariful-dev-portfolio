a308ff4adc4a2466e8c118516846e17ee09c5c3f
Ariful2634 <ariful2634@gmail.com>
2024-01-27 22:49:16 +0600

    added hover on navbar routing

--- src/Components/Home/Navbar.jsx
+++ src/Components/Home/Navbar.jsx
@@ -22,15 +22,15 @@ const Navbar = () => {
 
     const links = <>
         <li><NavLink to={'/'} className={({ isActive, isPending }) =>
-        isPending ? "pending" : isActive ? "text-[#FF444A] underline " : ""}>About</NavLink></li>
+        isPending ? "pending" : isActive ? "text-[#FF444A] underline " : "hover:text-[#FF444A]"}>About</NavLink></li>
         <li><NavLink to={'/skills'} className={({ isActive, isPending }) =>
-        isPending ? "pending" : isActive ? "text-[#FF444A] underline " : ""}>Skills</NavLink></li>
+        isPending ? "pending" : isActive ? "text-[#FF444A] underline " : "hover:text-[#FF444A]"}>Skills</NavLink></li>
         <li><NavLink to={'/exp'} className={({ isActive, isPending }) =>
-        isPending ? "pending" : isActive ? "text-[#FF444A] underline " : ""}>Experience</NavLink></li>
+        isPending ? "pending" : isActive ? "text-[#FF444A] underline " : "hover:text-[#FF444A]"}>Experience</NavLink></li>
         <li><NavLink to={'/projects'} className={({ isActive, isPending }) =>
-        isPending ? "pending" : isActive ? "text-[#FF444A] underline " : ""}>Projectss</NavLink></li>
+        isPending ? "pending" : isActive ? "text-[#FF444A] underline " : "hover:text-[#FF444A]"}>Projectss</NavLink></li>
         <li><NavLink to={'/contact'} className={({ isActive, isPending }) =>
-        isPending ? "pending" : isActive ? "text-[#FF444A] underline " : ""}>Contact</NavLink></li>
+        isPending ? "pending" : isActive ? "text-[#FF444A] underline " : "hover:text-[#FF444A]"}>Contact</NavLink></li>
     </>
 
     return (
@@ -41,7 +41,7 @@ const Navbar = () => {
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box  w-52 ">
+                        <ul tabIndex={0} className="font-bold dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box  w-52 ">
                            {links}
                         </ul>
                     </div>
